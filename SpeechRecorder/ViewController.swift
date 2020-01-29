4d739c0c67ce43bab282ad8fe0c2a94ed6259c45
Ivan Pylyp <ivan.stnsk@gmail.com>
2020-01-29 21:02:02 +0200

    Initial Commit

---
+++ SpeechRecorder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SpeechRecorder
+//
+//  Created by user on 1/29/20.
+//  Copyright © 2020 user. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
